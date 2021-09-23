160a89ba6a3084d01e8e20459eb0a6ad6639bf5b
abs007 <53483248+abs007@users.noreply.github.com>
2021-09-24 02:27:01 +0530

    Update Fibonacci.py

--- Memoization/Fibonacci.py
+++ Memoization/Fibonacci.py
@@ -1,7 +1,7 @@
 def fib(n,memo={}):
-    if n not in memo:
+    if n not in memo:      
         if n<=2:
-            return 1
-        
+            return 1 
         memo[n]= fib(n-1,memo)+ fib(n-2,memo)
+    
     return memo[n]
